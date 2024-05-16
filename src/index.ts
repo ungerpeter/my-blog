90db3f49926ac56fc5842ea2abe60c45002e508d
Peter Unger <peps.unger@gmail.com>
2024-05-16 10:46:32 +0200

    Use hono blog example code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { basicAuth } from "hono/basic-auth";
+import { prettyJSON } from "hono/pretty-json";
+import api from "./api";
+import { Bindings } from "./bindings";
 
-const app = new Hono()
+const app = new Hono();
+app.get("/", (c) => c.text("Pretty Blog API"));
+app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const middleware = new Hono<{ Bindings: Bindings }>();
+middleware.use("*", prettyJSON());
+middleware.use("/posts/*", async (c, next) => {
+  if (c.req.method !== "GET") {
+    const auth = basicAuth({
+      username: c.env.USERNAME,
+      password: c.env.PASSWORD,
+    });
+    return auth(c, next);
+  } else {
+    await next();
+  }
+});
 
-export default app
+app.route("/api", middleware);
+app.route("/api", api);
+
+export default app;
